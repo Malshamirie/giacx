ab1169564f08334de383b3c310678cb9d79a7c18
MohammedAbdo1 <mohammed775163@gmail.com>
2025-08-01 02:46:28 +0300

    first commit

--- resources/views/web/default/auth/register_includes/email_field.blade.php
+++ resources/views/web/default/auth/register_includes/email_field.blade.php
@@ -2,7 +2,6 @@
     <label class="input-label" for="email">{{ trans('auth.email') }} {{ !empty($optional) ? "(". trans('public.optional') .")" : '' }}:</label>
     <input name="email" type="text" class="form-control @error('email') is-invalid @enderror"
            value="{{ old('email') }}" id="email" aria-describedby="emailHelp">
-
     @error('email')
     <div class="invalid-feedback">
         {{ $message }}
